a2726cf022aa821e93891a4ddde5c459385ff9ba
Priscila Une <prixlin@gmail.com>
2025-02-04 17:06:11 -0300

    feat: create Home and Cart page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import ProductCard from "../components/ProductCard/ProductCard";
+
+export default function Home(){
+
+    return (
+        <div>
+           <p>Banner promoção </p>
+			<ProductCard />
+        </div>
+    )
+}
