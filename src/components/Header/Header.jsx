d49cb1c73cb184b446ae96ab3c0ba3a3e7f44f14
Priscila Une <prixlin@gmail.com>
2025-02-04 17:02:30 -0300

    feat: create Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,46 @@
 import { CircleUserRound, ShoppingCart, Search } from "lucide-react";
+import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
+import { CartContext } from "../../context/cart";
+import { useContext } from "react";
 
 export default function Header() {
+  const [showModal, setShowModal ] = useState(false);
+  const { cartItems } = useContext(CartContext);
+
+  const toggle = () => {
+    setShowModal(!showModal);
+  }
 
     return (
-      <header className="flex justify-between p-5 bg-slate-900 text-gray-100">
-        <h1>LOJINHA</h1>
+      <>
+       <header className="flex justify-between p-5 bg-slate-900 text-gray-100">
+          <Link to="/home">
+            <h1>LOJINHA</h1>
+          </Link>
 
-        <form action="" className="flex items-center gap-1 w-80">
-            <input type="text" className="w-full"/>
-            <Search />
-        </form>
+          <form action="" className="flex items-center gap-1 w-80">
+              <input type="text" className="w-full"/>
+              <Search />
+          </form>
 
-        <div className="flex gap-2">
-            <CircleUserRound />
-            <ShoppingCart />
-        </div>
+          <div className="flex gap-2">
+              <CircleUserRound />
+              <div className="flex">
+              <ShoppingCart className="cursor-pointer"  onClick={() => setShowModal(!open)}/>
+               <div className=" bg-red-400 rounded-lg w-4 h-4 text-xs flex justify-center text-center items-center"> {cartItems.length}</div>
+
+              </div>
+          </div>
+          
+          <Modal showModal={showModal} toggle={toggle} >
+
+            <p>dfahsjdfhausdhf </p>
+          </Modal >
         
-      </header>
+        </header>
+      </>
+     
     )
 }
