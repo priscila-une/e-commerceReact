3a0345b91ae3ea7e61770a7c3ff3eefbc2e15236
Priscila Une <prixlin@gmail.com>
2025-02-04 17:01:32 -0300

    feat: create Layout component

---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+
+export default function Layout(){
+    return (
+        <div>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
