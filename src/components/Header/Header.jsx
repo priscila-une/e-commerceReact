0f200f7a7b4c7d8028fa1a1a0a2bc36a6360568a
Priscila Une <prixlin@gmail.com>
2025-02-05 15:45:51 -0300

    feat: add cartItems on Modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import { CircleUserRound, ShoppingCart, Search } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import Modal from "../Modal/Modal";
 import { useState } from "react";
 import { CartContext } from "../../context/cart";
 import { useContext } from "react";
 
 export default function Header() {
-  const [showModal, setShowModal ] = useState(false);
-  const { cartItems } = useContext(CartContext);
+  const [ showModal, setShowModal ] = useState(false);
+  const { cartItems, addToCart, removeToCart, getCartTotal } = useContext(CartContext);
 
   const toggle = () => {
     setShowModal(!showModal);
@@ -28,15 +28,54 @@ export default function Header() {
           <div className="flex gap-2">
               <CircleUserRound />
               <div className="flex">
-              <ShoppingCart className="cursor-pointer"  onClick={() => setShowModal(!open)}/>
+              <ShoppingCart className="cursor-pointer"  onClick={() => setShowModal(true)}/>
                <div className=" bg-red-400 rounded-lg w-4 h-4 text-xs flex justify-center text-center items-center"> {cartItems.length}</div>
 
               </div>
           </div>
           
           <Modal showModal={showModal} toggle={toggle} >
+            {/* <CartItems /> */}
+            <div className="text-slate-900">
+              <h3 className="font-bold">Meu carrinho ({cartItems.length})</h3>
+              <div className="flex flex-col gap-8 p-4 items-center">
+                {cartItems && cartItems.map((item) => (
+                  <div className="flex justify-between items-center gap-4" key={item.id}>
+                    <img src={item.thumbnail} alt={item.title} className="w-12"/>
+                    <p className="font-bold">{item.title}</p>
+                    <p className="font-semibold">{item.price}</p>
 
-            <p>dfahsjdfhausdhf </p>
+                    <div className="flex items-center justify-center gap-2">
+                      <button className="border border-slate-900 w-4 items-center rounded-sm"
+                        onClick={()=> addToCart(item)} 
+                      > + </button>
+                      <span>{item.quantity}</span>
+                      <button className="border border-slate-900 w-4 items-center rounded-sm"
+                        onClick={()=> removeToCart(item)}> - </button>
+                    </div>
+                    
+                  </div>
+                ))}
+                {cartItems.length > 0 ? (
+                  <>
+                   <h3 className="font-bold">Total ${getCartTotal()} </h3>
+                   <div className="flex justify-between gap-4">
+                    <button className="border bg-red-300 p-2 rounded-sm font-semibold"
+                      onClick={toggle}>Fechar</button>
+                    <button className="border bg-green-300 p-2 rounded-sm font-semibold"
+                      onClick={() => {
+                        setShowModal(false);
+                        redirect("/cart")
+                      }}
+                    >finalizar pedido</button>
+                   </div>
+                   
+                   </>
+                ) : (
+                  <h3 className="font-bold"> Carrinho de compras vazio. </h3>
+                )}
+              </div>
+            </div>
           </Modal >
         
         </header>
