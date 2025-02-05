79428e3658769f143cc5d7407b63ac9bd22138a5
Priscila Une <prixlin@gmail.com>
2025-02-05 15:43:40 -0300

    feat: add addToCart function

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect, useContext } from 'react'
+import { CartContext } from '../../context/cart';
 
 export default function ProductCard() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const { addToCart } = useContext(CartContext);
+
 
   async function getProducts() {
     try {
-        const response = await fetch('https://fakestoreapi.com/products')
+        const response = await fetch("https://dummyjson.com/products")
         const data = await response.json()
 
-        setProducts(data)
+        setProducts(data.products)
     } catch (error) {
         console.log(error)
     }
@@ -27,13 +29,19 @@ export default function ProductCard() {
                  {
                     products.map(product => (
                       <div key={product.id} className='bg-slate-200 shadow-md rounded-lg px-10 py-10 flex flex-col items-center'>
-                        <img src={product.image} alt={product.title} className='rounded-md h-48' />
-                        <div>
+                        <img src={product.thumbnail} alt={product.title} className='rounded-md h-48' />
+                        <div className='flex flex-col items-center'>
                           <h1 className='text-lg font-bold text-slate-800'>{product.title}</h1>
                           <p className='mt-2 text-gray-600'>${product.price}</p>
                         </div>
                         <div className='mt-6 flex justify-between items-center'>
-                          <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'>Adiconar no Carrinho</button>
+                          {product.stock > 0 ? (
+                             <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
+                             onClick={() => addToCart(product)}
+                           >Adiconar no Carrinho</button>  
+                          ) : (
+                            <div className='font-bold text-red-700'>Fora de Estoque</div>
+                          )}
                         </div>
                       </div>
                     ))
@@ -45,9 +53,9 @@ export default function ProductCard() {
                     Ops.. estamos sem produto... 
                 </div>
             )}
-             
-       
       </div>
+
+      
     </div>
   )
 }
